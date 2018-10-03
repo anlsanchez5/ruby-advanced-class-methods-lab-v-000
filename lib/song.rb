a749a13d8703b8e269713bb317955b3826986f73
Angelica Sanchez <anlsanchez@ucdavis.edu>
2018-10-03 06:10:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,10 +36,10 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if self.find_by_name(name) == true
-      return self
-    else
+    if !self.find_by_name(name)
       self.create_by_name(name)
+    else
+      self
     end
   end
 end
