b95cb3c311865f5cf8169349c2f82db3824b6848
Angelica Sanchez <anlsanchez@ucdavis.edu>
2018-10-03 05:41:26 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +11,36 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    @@all << self.new
+    @@all.last
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    @name = name
+    song.name = @name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    @name = name
+    song.name = @name
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == true
+      return self.name
+    else
+      self.create_by_name(name)
+binding.pry
+    end
+  end
 end
